b47a0a920c066238fc26f5b88ccd7db514eff502
ryan23052008 <ryandiasvieira123@gmail.com>
2025-04-26 10:08:39 -0300

    first commit

---
+++ src/main/java/model/Aluno.java
@@ -0,0 +1,49 @@
+package model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Aluno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nomeAluno;
+    private String emailAluno;
+    private String telefoneAluno;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNomeAluno() {
+        return nomeAluno;
+    }
+
+    public void setNomeAluno(String nomeAluno) {
+        this.nomeAluno = nomeAluno;
+    }
+
+    public String getEmailAluno() {
+        return emailAluno;
+    }
+
+    public void setEmailAluno(String emailAluno) {
+        this.emailAluno = emailAluno;
+    }
+
+    public String getTelefoneAluno() {
+        return telefoneAluno;
+    }
+
+    public void setTelefoneAluno(String telefoneAluno) {
+        this.telefoneAluno = telefoneAluno;
+    }
+}
